0ee89eea4411d65a2fab7587dae9a0a94409c349
ygngzln <ygngzln@gmail.com>
2022-03-31 21:41:19 -0700

    New file setup

--- backend/backend.js
+++ backend/backend.js
@@ -3,5 +3,9 @@ const dotenv = require('dotenv').config({path:__dirname+'/.env'})
 const port = process.env.PORT || 3000
 
 const app = express()
+app.get('/api/goals', (req, res) => {
+    // res.send("GET GOOD L")
+    res.status(200).json({"12908310293012": 'the cake'})
+})
 
 app.listen(port, () => console.log(`Server listening at port ${port}`))
