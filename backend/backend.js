d02a2177fcda53ab647795467bee90198c9a5f50
ygngzln <ygngzln@gmail.com>
2022-04-03 12:19:01 -0700

    favico added

--- backend/backend.js
+++ backend/backend.js
@@ -16,7 +16,6 @@ app.use('/api/swords', require('./routes/route'))
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')))
-
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 }else{
     app.get('/', (req, res) => res.send('Not production'))
--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const fig = await mongoose.connect(process.env.MONGO)
         console.log(`MongoDB has been connnected at ${fig.connection.host}`)
     } catch (e){
+        console.log("MONGODB ERROR!!!!")
         console.log(e)
         process.exit(1)
     }
